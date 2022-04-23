867bed079e042630970792ef939d0d063c51644d
Liszt-Fly <1904231450@qq.com>
2022-04-23 23:55:23 +0800

    bug } error

--- src/MikeHashMap/MikeHashTable.java
+++ src/MikeHashMap/MikeHashTable.java
@@ -14,62 +14,54 @@ public class MikeHashTable {
 
 
     }
-
     //Bucket的容器，槽位，初始为5个
     private LinkedList<Entry>[] entries = new LinkedList[5];
-
-    public String get(int key) {
-        var entry = getEntry(key);
-        return entry == null ? null : entry.value;
+    public String get(int key){
+        var entry=getEntry(key);
+        return  entry==null?null:entry.value;
     }
-
     public void put(int key, String value) {
 
-        var entry = getEntry(key);
-        if (entry != null) {
-            entry.value = value;
+        var entry=getEntry(key);
+        if(entry!=null) {
+            entry.value=value;
             return;
         }
-        var bucket = getOrCreateBucket(key);
-        bucket.addLast(new Entry(key, value));
+        var bucket=getOrCreateBucket(key);
+        bucket.addLast(new Entry(key,value));
 
     }
-
-    private LinkedList<Entry> getBucket(int key) {
-        return entries[hash(key)];
+    private LinkedList<Entry>getBucket(int key){
+        return  entries[hash(key)];
     }
-
-    private Entry getEntry(int key) {
-        var bucket = entries[key];
-        if (bucket == null) return null;
-        for (var entry : bucket) {
-            if (entry.key == key) {
-                return entry;
+    private Entry getEntry(int key){
+        var bucket=entries[key];
+        if(bucket==null) return null;
+        for(var entry:bucket){
+            if(entry.key==key){
+                return  entry;
             }
         }
-        return null;
+        return  null;
     }
-
     private LinkedList<Entry> getOrCreateBucket(int key) {
-        var bucket = getBucket(key);
-        if (bucket == null) {
-            bucket = new LinkedList<>();
+        var bucket=getBucket(key);
+        if(bucket==null) {
+            bucket=new LinkedList<>();
         }
-        return bucket;
+        return  bucket;
     }
-
     private int hash(int key) {
         return key % entries.length;
     }
 
-    public void remove(int key) {
+    public void remove(int key){
         //先找到对应的bucket
-        var entry = getEntry(key);
-        var bucket = getBucket(key);
-        if (entry == null) throw new IllegalStateException();
+        var entry=getEntry(key);
+        var bucket=getBucket(key);
+        if(entry==null)throw new IllegalStateException();
         getBucket(key).remove(key);
-        throw new IllegalStateException();
+            throw new IllegalStateException();
 
+        }
     }
-}
-}
